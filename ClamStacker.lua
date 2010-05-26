73744c19dfd4827b879fa2cfbf5b91178e171f20
Mitchell Morris <mitchellmorris@mitchell-morriss-imac.local>
2010-05-25 20:01:14 -0700

    Added the bloated fish for the daily fishing quests in Shattrath and Dalaran

--- ClamStacker.lua
+++ ClamStacker.lua
@@ -1,6 +1,6 @@
 ClamStacker = LibStub("AceAddon-3.0"):NewAddon("ClamStacker", "AceConsole-3.0", "AceEvent-3.0", "AceBucket-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("ClamStacker", false)
-local version = "1.0.17"
+local version = "1.0.18"
 
 local debugFrame = tekDebug and tekDebug:GetFrame("ClamStacker")
 
@@ -28,6 +28,8 @@ local clamItemIds = {
     ["27513"] = true,
     ["44475"] = true,
     ["21150"] = true,
+    ["35313"] = true,
+    ["45328"] = true,
 
     -- Noblegarden eggs
     ["45072"] = true,
