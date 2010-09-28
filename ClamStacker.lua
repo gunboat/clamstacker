7382ef9d559e9d50c4019294fc466d47655cf054
Mitchell Morris <mitchellmorris@mitchell-morriss-imac.local>
2010-09-27 19:41:57 -0500

    Added phantom ghostfish

--- ClamStacker.lua
+++ ClamStacker.lua
@@ -1,6 +1,6 @@
 ClamStacker = LibStub("AceAddon-3.0"):NewAddon("ClamStacker", "AceConsole-3.0", "AceEvent-3.0", "AceBucket-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("ClamStacker", false)
-local version = "1.1.9"
+local version = "1.1.10"
 
 local debugFrame = tekDebug and tekDebug:GetFrame("ClamStacker")
 
@@ -49,6 +49,7 @@ local clamItemIds = Set {
     45072,
     45328,
     45724,
+    45902,
     45909,
     46007,
     46740,
