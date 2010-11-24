5f8ddd46b6eef48167fa7f62447412bfd215ba01
gunboat <gunboat.smith.2010@gmail.com>
2010-11-24 00:25:29 -0600

    Added bag of shiny things

--- ClamStacker.lua
+++ ClamStacker.lua
@@ -1,6 +1,6 @@
 ClamStacker = LibStub("AceAddon-3.0"):NewAddon("ClamStacker", "AceConsole-3.0", "AceEvent-3.0", "AceBucket-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("ClamStacker", false)
-local version = "1.2.6"
+local version = "1.2.7"
 
 local debugFrame = tekDebug and tekDebug:GetFrame("ClamStacker")
 
@@ -75,6 +75,7 @@ local clamItemIds = Set {
     54516,
     54535,
     54536,
+    67414,
 }
 
 -- This will be filled in once we have I18N loaded
