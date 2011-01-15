4660376d2756074a72e3b1cb201d07acf6cf0fdb
gunboat <gunboat.smith.2010@gmail.com>
2011-01-15 02:12:06 -0600

    Added bulging sack of gold

--- ClamStacker.lua
+++ ClamStacker.lua
@@ -1,6 +1,6 @@
 ClamStacker = LibStub("AceAddon-3.0"):NewAddon("ClamStacker", "AceConsole-3.0", "AceEvent-3.0", "AceBucket-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("ClamStacker", false)
-local version = "1.2.9"
+local version = "1.2.10"
 
 local debugFrame = tekDebug and tekDebug:GetFrame("ClamStacker")
 
@@ -76,6 +76,7 @@ local clamItemIds = Set {
     54516,
     54535,
     54536,
+    62062,
     67414,
     67443,
 }
