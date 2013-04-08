c39269cb22047ebbc0d591b38cf63413561427ee
Gunboat Smith <gunboat.smith.2010@gmail.com>
2013-04-07 21:45:03 -0500

    releasing as v1.3.11

--- ClamStacker.lua
+++ ClamStacker.lua
@@ -1,6 +1,6 @@
 ClamStacker = LibStub("AceAddon-3.0"):NewAddon("ClamStacker", "AceConsole-3.0", "AceEvent-3.0", "AceBucket-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("ClamStacker", false)
-local version = "1.3.10"
+local version = "1.3.11"
 
 local debugFrame = tekDebug and tekDebug:GetFrame("ClamStacker")
 
