1c7854de831c7854101342570ece4849e80b4670
Gunboat Smith <gunboat.smith.2010@gmail.com>
2013-09-11 20:54:40 -0500

    releasing as v1.4.7 since I forgot one file

--- ClamStacker.lua
+++ ClamStacker.lua
@@ -1,6 +1,6 @@
 ClamStacker = LibStub("AceAddon-3.0"):NewAddon("ClamStacker", "AceConsole-3.0", "AceEvent-3.0", "AceBucket-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("ClamStacker", false)
-local version = "1.4.6"
+local version = "1.4.7"
 
 local debugFrame = tekDebug and tekDebug:GetFrame("ClamStacker")
 
